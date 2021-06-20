6342bbd4016343b6ad5e983839da394feba789bd
The Floating String <laurencel2001@gmail.com>
2021-06-19 23:21:41 -0400

    Integrate Keras into REST API

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ from PIL import Image
 import io
 import numpy as np
 
+from keras.models import load_model
+
+
+model = load_model("static/results/model.h5")
+
 app = FastAPI()
 
 @app.get("/")
@@ -19,4 +24,6 @@ async def uploadPhoto(file: UploadFile = File(...)):
 	image_array = cv2.resize(image_array, dsize=(720, 720), interpolation=cv2.INTER_CUBIC)
 	image_array = cv2.cvtColor(image_array, cv2.COLOR_BGR2RGB)
 
-	return {"filename": file.filename}
+	calories = model.predict(np.array([image_array]))
+
+	return {"filename": file.filename, "calories": float(calories)}
