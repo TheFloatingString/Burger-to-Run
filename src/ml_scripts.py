d45ad63a63975604999614afc44e63fdadb3d5dc
The Floating String <laurencel2001@gmail.com>
2021-06-19 23:01:05 -0400

    Create functional CNN

--- src/ml_scripts.py
+++ src/ml_scripts.py
@@ -3,6 +3,10 @@ import numpy as np
 
 import os
 
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+
 
 PATH = "static/data"
 
@@ -23,7 +27,7 @@ for filename in list_of_files:
 	print(img.shape)
 	print(res.shape)
 
-	cv2.imshow('image window', res)
+	# cv2.imshow('image window', res)
 	cv2.waitKey(0)
 
 X_data = np.array(X_data)
@@ -31,3 +35,32 @@ y_data = np.array(y_data)
 
 print(X_data.shape)
 print(y_data.shape)
+
+
+model = Sequential()
+
+model.add(Conv2D(32, (3,3), input_shape=(720, 720, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['mse'])
+
+model.fit(
+        X_data, y_data,
+        epochs=20,
+        verbose=1)
+
+model.save_weights("static/results/model.h5")
